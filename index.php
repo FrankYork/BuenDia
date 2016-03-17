1e84963ee8d647a841591dc410099bb3739a1dd0
alfredo garcia <frankgv.2015@gmail.com>
2016-03-17 08:14:53 -0500

    buen dia

---
+++ index.php
@@ -0,0 +1,39 @@
+<html lang="es" class="os-win">
+<head>
+    <title>Regístrate | CodeYork </title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Regístrate</title>
+    <link rel="shortcut icon" href="public/logo/logo-tittle.ico">
+</head>
+
+<body dir="ltr" class="guest v2 uno-body chrome-v5 chrome-v5-responsive sticky-bg guest js">
+<div class="container">
+   <link rel="stylesheet" href="https://static.licdn.com/sc/h/1ilhol6x250ko13uzf9th6yw8">
+   <link rel="stylesheet" href="https://static.licdn.com/sc/h/6cchmk79en52g9rlzt0fuhvr1,ds7noc8t34vk4bryozik4pcmh">
+</div>
+
+<div class=""><div class="content-wrapper using-single-form"><div class="fodal join-container reg-content-container uno-anchor" data-li-widget-status="jsdone"><div class="fodal-wrapper">
+<div class="header-container">
+<header>
+<h1 class="container-fluid"><img src="public/imagen/Titulo.png" width="500"></h1>
+</header>
+<h2 class="title">Únete ahora... Únete Ya !!!</h2>
+</div>
+<div class="content-container"><div class="reg-content-wrapper single"><div class="top-pane"><img class="profile-photo" alt="Frank York Garay Vela" src="public/imagen/Logo.png" width="300" height="100"><h3 class="message has-photo">Únete a CodeYork para ver el perfil completo de Frank. ¡Es gratis!</h3></div>
+<div class="join-form-container form-container"><div class="join-form-wrapper form-wrapper"><div class="alerts-cont" data-li-error-translated="" data-li-error-debug=""></div> <div class="loading-indicator hidden"></div><form class="join-linkedin-form float-label-form" action="/start/reg/api/createAccount" method="POST" data-jsenabled="check" novalidate=""><fieldset><legend>Únete a CodeYork</legend><ul class="input-fields container-box"><li class="first-name-field cell cell-input"><div class="first-name-wrapper cell-body"><label for="first-name">Nombres</label><input class="cell-body-textinput" name="firstName" id="first-name" type="text" aria-required="true" data-li-bingo-validation="control" value="" autofocus="&quot;autofocus&quot;/"></div></li><li class="last-name-field cell cell-input"><div class="last-name-wrapper cell-body"><label for="last-name">Apellidos</label><input class="cell-body-textinput" name="lastName" id="last-name" type="text" aria-required="true" value="" data-li-bingo-validation="control"></div></li><li class="email-field cell cell-input"><div class="email-wrapper cell-body"><label for="join-email">Correo electrónico</label><input class="cell-body-textinput" type="email" autocapitalize="off" aria-required="true" id="join-email" name="emailAddress" value=""></div></li><li class="password-field cell cell-input"><div class="password-wrapper cell-body"><label for="join-password">Contraseña (6 o más caracteres)</label><input class="cell-body-textinput" type="password" autocomplete="off" aria-required="false" id="join-password" name="password"></div></li></ul>
+<p class="disclosure-text">Al hacer clic en <span id="disclosure-text">Únete ahora</span>, aceptas las <a class="tapable" href="">Condiciones de uso</a>, la <a class="tapable" href="">Política de privacidad</a> y la <a class="tapable" href="">Política de cookies</a> de CodeYork.</p>
+
+<button type="button" class="btn btn-primary join-btn" data-position-join="right"><span class="fill-v2">Únete ahora</span></button>
+
+</fieldset><input type="hidden" name="isJsEnabled" value="true"><input type="hidden" name="trcode" value="reg-cold-signup-home"><input type="hidden" name="regCsrf" value=""><input type="hidden" name="referer" id="referer" value=""><input type="hidden" name="source" id="source" value=""><input type="hidden" name="referrerPageKey" id="referrerPageKey" value="reg-cold-signup-home"><input type="hidden" name="csrfToken" value="ajax:7174135673245716153"><input name="postOnboardingRedirectUrl" id="post-onboarding-redirect-url" type="hidden" value=""></form><div class="fb-btn-container"><p class="reg-option"><span class="line-wrapper"><span class="line"></span></span><span class="content"><span>o</span></span></p>
+
+<button type="button" class="fb-btn" data-genie
+-url="" data-position-facebook="bottom"><span class="fb-btn-content">Continuar con Facebook</span></button>
+
+</div><p class="signin-link">¿Ya eres usuario de CodeYork? <a href="">Inicia sesión</a></p></div></div>
+</div><div class="footer"><div class="wrapper"><p class="copyright">CodeYork Corporation © 2016</p></div></div></div></div></div></div><code id="countryCodeMap" style="display: none;"></code></div>
+
+
+
+</body>
+</html>
